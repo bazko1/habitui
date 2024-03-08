0613790470eaffd64fa8e2beec178cf51fd51a59
bazko1 <bazyli.kot.dev@gmail.com>
2024-03-09 00:05:51 +0100

    implement weekly summary completion counting

--- task_statistics.go
+++ task_statistics.go
@@ -4,8 +4,50 @@ import (
 	"time"
 )
 
+const WeekDuration int = 7
+
 // MonthlyTaskCompletion is a record of task completion for each month.
 type MonthlyTaskCompletion map[time.Month][]time.Time
 
 // YearlyTaskCompletion keeps a record of MonthlyTaskCompletion for each year.
 type YearlyTaskCompletion map[int]MonthlyTaskCompletion
+
+// Returns number of completion over the week represented by given date.
+func (task Task) WeekCompletion(year int, month time.Month, day int) int {
+	mcp := task.MonthCompletionTime(year, month)
+	if mcp == nil {
+		return 0
+	}
+	// we count all completions that are less than end
+	end := time.Date(year, month, day, 23, 59, 59, 59, task.GetTime().Location())
+
+	weekDay := int(end.Weekday())
+	// counting Sunday as seventh day of the week
+	if weekDay == 0 {
+		weekDay = 7
+	}
+
+	weekBegin := time.Date(year, month, day, 0, 0, 0, 1, task.GetTime().Location()).AddDate(0, 0, -weekDay)
+
+	counter := 0
+
+	for _, cdt := range mcp {
+		if cdt.Before(weekBegin) {
+			continue
+		}
+
+		if cdt.After(end) {
+			break
+		}
+
+		counter++
+	}
+
+	return counter
+}
+
+func (task Task) CurrentWeekCompletion() int {
+	y, m, d := task.GetTime().Date()
+
+	return task.WeekCompletion(y, m, d)
+}
--- task_test.go
+++ task_test.go
@@ -59,8 +59,8 @@ func TestTaskWithChangingDay(t *testing.T) {
 	dit.AddDay()
 	task.MakeTaskCompleted()
 
-	y, m, _ := dit.CurrentTime.Date()
-	monthCompletions := task.MonthCompletionTime(y, m)
+	year, month, _ := dit.CurrentTime.Date()
+	monthCompletions := task.MonthCompletionTime(year, month)
 
 	if len(monthCompletions) != 3 {
 		t.Fatalf("Task completion is %d when it should be 3", len(monthCompletions))
@@ -75,5 +75,21 @@ func TestTaskWithChangingDay(t *testing.T) {
 	// 2 days passed and task wasn't completed so strike should be zeroed.
 	if task.CurrentStrike() != 0 {
 		t.Fatalf("Task strike is %d when it should be 0", task.CurrentStrike())
+	}
+
+	dit.AddDay()
+	dit.AddDay()
+	// this moves date to sunday
+	dit.AddDay()
+
+	if task.CurrentWeekCompletion() != 3 {
+		t.Fatalf("Task should be completed 3 times this week while it returned %d", task.CurrentStrike())
+	}
+
+	// this moves date to monday so the weekly counter should be 0 now
+	dit.AddDay()
+
+	if task.CurrentWeekCompletion() != 0 {
+		t.Fatalf("Task should be completed 0 times this week while it returned %d", task.CurrentStrike())
 	}
 }
