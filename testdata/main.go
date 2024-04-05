de0fd48593473cc13e7f26db398417fd187d796a
bazko1 <bazyli.kot.dev@gmail.com>
2024-04-05 15:45:56 +0200

    add data generation script

---
+++ testdata/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	habitui "github.com/bazko1/habitui/habit"
+	"math/rand/v2"
+)
+
+func main() {
+	startDate := time.Date(2024, 3, 1, 12, 0, 0, 0, time.Local)
+	now := func() time.Time {
+		return startDate
+	}
+	tasks := habitui.TaskList{
+		habitui.NewTaskWithCustomTime("work on habittui", "daily app grind", now),
+		habitui.NewTaskWithCustomTime("go for a walk", "walking is relaxing and healthy activity", now),
+	}
+
+	for startDate.Before(time.Now().AddDate(0, 0, -2)) {
+		startDate = startDate.AddDate(0, 0, 1)
+
+		tasks[0].MakeTaskCompleted()
+
+		for i := 1; i < len(tasks); i++ {
+			if rand.Int()%2 != 0 {
+				tasks[i].MakeTaskCompleted()
+			}
+		}
+	}
+
+	err := habitui.JSONSaveTasks(".habitui.json", tasks)
+	if err != nil {
+		fmt.Println("failed to save tasks state")
+	}
+}
