2f06358d3560ba6d8738b94bd9374f00a34832fe
bazko1 <bazyli.kot.dev@gmail.com>
2024-04-12 23:22:28 +0200

    feat: add option functions to server

--- server/server.go
+++ server/server.go
@@ -3,27 +3,55 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 const (
-	defaultPort = 3000
-	defaultHost = "localhost"
+	DefaultPort                      = 3000
+	DefaultHost                      = "localhost"
+	DefaultReadTimeout time.Duration = 100 * time.Millisecond
 )
 
 type Option func(*Config) error
 
+func WithHost(host string) Option {
+	return func(c *Config) error {
+		c.host = host
+
+		return nil
+	}
+}
+
+func WithPort(port int) Option {
+	return func(c *Config) error {
+		c.port = port
+
+		return nil
+	}
+}
+
+func WithReadTimeout(timeout time.Duration) Option {
+	return func(c *Config) error {
+		c.readTimeout = timeout
+
+		return nil
+	}
+}
+
 type Config struct {
-	host string
-	port int
+	host        string
+	port        int
+	readTimeout time.Duration
 }
 
 func DefaultConfig() Config {
 	return Config{
-		host: defaultHost,
-		port: defaultPort,
+		host:        DefaultHost,
+		port:        DefaultPort,
+		readTimeout: DefaultReadTimeout,
 	}
 }
 
@@ -42,9 +70,14 @@ func New(opts ...Option) (*http.Server, error) {
 		}
 	}
 
-	http.ListenAndServe(fmt.Sprintf("%s:%d", c.host, c.port), r)
-	server := &http.Server{Addr: fmt.Sprintf("%s:%d", c.host, c.port), Handler: r}
-	server.ListenAndServe()
+	server := &http.Server{
+		Addr:        fmt.Sprintf("%s:%d", c.host, c.port),
+		Handler:     r,
+		ReadTimeout: c.readTimeout,
+	}
+	if err := server.ListenAndServe(); err != nil {
+		return nil, fmt.Errorf("failed to listen and serve: %w", err)
+	}
 
 	return server, nil
 }
