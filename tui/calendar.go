824e9cbc34ffca23b8155a53ae1d11322a88a8bb
bazko1 <bazyli.kot.dev@gmail.com>
2024-04-26 23:30:05 +0200

    feat: minor improvements with calendar panel creation

--- tui/calendar.go
+++ tui/calendar.go
@@ -3,15 +3,20 @@ package tui
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
+	"github.com/bazko1/habitui/habit"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/lipgloss/table"
 )
 
-func renderCalendar() {
-	now := time.Now()
+// renderCalendar
+// TODO: this will render calendar that will be added as separate panel to tui
+// calendar will be different for each task as completed days will be colored in green.
+func renderCalendar(task habit.Task) string {
+	now := task.GetTime()
 	days := getDaysInMonth(time.Now())
 	firstDay := time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, time.UTC)
 	firstDayWeekday := int(firstDay.Weekday())
@@ -20,19 +25,21 @@ func renderCalendar() {
 	for i := firstDayWeekday; i > 0; i-- {
 		daysSlice = append(daysSlice, fmt.Sprintf("%d", firstDay.AddDate(0, 0, -i).Day()))
 	}
+
 	for i := 1; i <= days; i++ {
-		daysSlice = append(daysSlice, fmt.Sprintf("%d", i))
+		daysSlice = append(daysSlice, strconv.Itoa(i))
 	}
-	// numWeeks := days/7 + 1
-	// weeksLabels :=
+
 	re := lipgloss.NewRenderer(os.Stdout)
 	labelStyle := re.NewStyle().Foreground(lipgloss.Color("241"))
 
 	weeks := [][]string{}
+
 	var i int
 	for ; i < days/7; i++ {
 		weeks = append(weeks, daysSlice[i*7:i*7+7])
 	}
+
 	weeks = append(weeks, daysSlice[i*7:])
 
 	t := table.New().
@@ -40,12 +47,12 @@ func renderCalendar() {
 		BorderRow(true).
 		BorderColumn(true).
 		Rows(weeks...).
-		StyleFunc(func(row, col int) lipgloss.Style {
+		StyleFunc(func(_, _ int) lipgloss.Style {
 			return lipgloss.NewStyle().Padding(0, 1)
 		})
 
-	// files := labelStyle.Render(strings.Join([]string{" 1", "2", "3", "4", "5", "6", "7", "8"}, "\n\n "))
-
 	dayNames := labelStyle.Render(strings.Join([]string{" Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"}, "  "))
-	fmt.Println(lipgloss.JoinVertical(lipgloss.Left, dayNames, t.Render()) + "\n")
+	calendar := lipgloss.JoinVertical(lipgloss.Left, dayNames, t.Render()) + "\n"
+
+	return calendar
 }
